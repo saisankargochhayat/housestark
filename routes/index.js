81cc235c517e31fdd072a5ed213caf2bd15e06af
Sandeep <sandeep.mahapatra.5@gmail.com>
2017-09-02 16:57:59 +0530

    ejs error fixed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log('got get req');
+  res.sendFile(path.resolve(__dirname + '/../public/tindex.html'));
 });
 
 module.exports = router;
